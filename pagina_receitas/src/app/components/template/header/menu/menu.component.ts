87c00f752cbaa21cbc84e2091232b284e6d9c7ab
Murilo Elias <106710958+iloElias@users.noreply.github.com>
2023-06-17 14:28:36 -0300

    Quinto Comit, ajuste nav-bar.

--- pagina_receitas/src/app/components/template/header/menu/menu.component.ts
+++ pagina_receitas/src/app/components/template/header/menu/menu.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuPanel } from '@angular/material/menu';
 
 @Component({
   selector: 'app-menu',  
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-
 export class MenuComponent {
+  @ViewChild('menuRef') menuRef!: MatMenuPanel<any>; // Referencia necessaria (gambiarra)
 
+  menus = [
+    { label: 'Comidas Brasileiras', ref: 'comidasBrasileiras', items: ['Acarajé', 'Arroz carreteiro', 'Feijoada', 'Frango com quiabo', 'Galinhada com pequi', 'Picadinho de carne', 'Porco à pururuca', 'Virado à paulista', 'Outros'] },
+    { label: 'Bolos e Tortas', ref: 'bolosTortas', items: ['Template', 'Template', 'Template'] },
+    { label: 'Bebidas', ref: 'bebidas', items: ['Template', 'Template', 'Template'] },
+    { label: 'Carnes', ref: 'carnes', items: ['Template', 'Template', 'Template'] },
+    { label: 'Massas', ref: 'massas', items: ['Template', 'Template', 'Template'] },
+    { label: 'Sopas', ref: 'sopas', items: ['Template', 'Template', 'Template'] },
+    { label: 'Doces', ref: 'doces', items: ['Template', 'Template', 'Template'] },
+    { label: 'Sobremesas', ref: 'sobremesas', items: ['Template', 'Template', 'Template'] },
+    { label: 'Alimentos Saudáveis', ref: 'fitnessFood', items: ['Template', 'Template', 'Template'] }
+  ];
 }
