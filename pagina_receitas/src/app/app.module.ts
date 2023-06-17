e1ab048744801cec5137fb55928bbb717f331aa6
gab-2 <107018299+gab-2@users.noreply.github.com>
2023-06-17 00:50:19 -0300

    ThirdCommit

--- pagina_receitas/src/app/app.module.ts
+++ pagina_receitas/src/app/app.module.ts
@@ -7,19 +7,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MenuComponent } from './components/template/header/menu/menu.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BodyComponent } from './components/template/header/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+ 
+}
