27bdbfda73bb8e336e8abdec099f1f4ab670597a
Murilo Elias <106710958+iloElias@users.noreply.github.com>
2023-06-21 01:09:12 -0300

    Cadastro de receita
    Modificações na tela de cadastro de receita.

--- pagina_receitas/src/app/paginas/MinhasReceitas/MinhasReceitas.component.ts
+++ pagina_receitas/src/app/paginas/MinhasReceitas/MinhasReceitas.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Image {
+  url: string;
+}
+
 @Component({
   selector: 'app-MinhasReceitas',
   templateUrl: './MinhasReceitas.component.html',
   styleUrls: ['./MinhasReceitas.component.css']
 })
-export class MinhasReceitasComponent implements OnInit {
 
+
+export class MinhasReceitasComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
   }
 
+  images: string[] = [];
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.images.push(e.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  removeImage(image: string) {
+    const index = this.images.indexOf(image);
+    if (index !== -1) {
+      this.images.splice(index, 1);
+    }
+  }
 }
