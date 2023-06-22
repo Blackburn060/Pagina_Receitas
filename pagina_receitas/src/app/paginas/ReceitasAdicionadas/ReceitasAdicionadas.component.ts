938d8ad81650d019050f71da469a228d8b5f2dc1
gab-2 <107018299+gab-2@users.noreply.github.com>
2023-06-21 23:40:37 -0300

    Ajustes Pagina ReceitasAdicioadas

--- pagina_receitas/src/app/paginas/ReceitasAdicionadas/ReceitasAdicionadas.component.ts
+++ pagina_receitas/src/app/paginas/ReceitasAdicionadas/ReceitasAdicionadas.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ReceitasAdicionadas.component.css']
 })
 export class ReceitasAdicionadasComponent implements OnInit {
+  dataSource: any[] = [
+    { name: 'Item 1', quantity: 10, price: 100 },
+    { name: 'Item 2', quantity: 5, price: 50 },
+    // ...
+  ];
+
+  displayedColumns: string[] = ['name', 'quantity', 'price'];
 
 
   constructor() { }
@@ -14,5 +21,6 @@ export class ReceitasAdicionadasComponent implements OnInit {
   ngOnInit() {
 
   }
-
 }
+
+
